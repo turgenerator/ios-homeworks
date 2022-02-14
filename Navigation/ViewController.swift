5ecac0630cd612a1b472e794ae6d65b578095649
Nikita Tyrgenev <turgenerator@gmail.com>
2022-02-14 13:05:22 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Nikita Turgenev on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
